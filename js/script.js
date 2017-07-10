bd4eefb50663f0a128cdc5a320777f035b6f3bd4
YuriyShandor <yuriyshandor@gmail.com>
2017-07-10 15:27:06 +0300

    Add Section Banner

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function(){
+  $(".small_button").click(function(){
+    $(".small_button").removeClass("small_button_click");
+    $(this).addClass("small_button_click");
+    /*var index = $(this).index()+1;
+    $(".content").removeClass("show");
+    $(".content:nth-child("+ index +")").addClass("show");*/
+    });
+  });
